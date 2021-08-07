1c480ab5adc03b8780015504e8de2973ad6d557e
abhaysinghs772 <abhaysinghs772@gmail.com>
2021-08-07 10:33:22 +0530

    debugging

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+// const peaceSongsLists = ['a', 'b', 'c']
+// const joySongsList = ['x', 'y', 'z']
+// const energySongsLists = ['d', 'e', 'f']
+
+const bhajans = {
+  peaceSongsLists: [
+    'a',
+    'b',
+    'c'
+  ],
+  joySongsList: [
+    'x',
+    'y',
+    'z'
+  ],
+  energySongsLists: [
+    'd',
+    'e',
+    'f'
+  ]
+}
+
 function App() {
+  let [songs, setSongs] = useState("");
+
+  function peaceHandeler() {
+
+    // console.log("peace");
+    let peaceSongs = bhajans.peaceSongsLists;
+    // console.log(peaceSongs);
+    setSongs(peaceSongs);
+    console.log(songs)
+  }
+
+  function joyHandeler() {
+    console.log("joy");
+  }
+
+  function energyHandeler() {
+    console.log("energy");
+  }
+
   return (
     <div className="App">
-      <h1 className= "heading" >Bhajans</h1>
+      <h1 className="heading" >My favourite Bhajans</h1>
       <p>listen to my favourite Bhajans. select any catogery below</p>
+      <button className="btn" onClick={() => peaceHandeler()} >peace</button>
+      <button className="btn" onClick={() => joyHandeler()} >joy</button>
+      <button className="btn" onClick={() => energyHandeler()} >energy</button>
+      <hr></hr>
+      <div>
+
+      </div>
     </div>
   );
 }
